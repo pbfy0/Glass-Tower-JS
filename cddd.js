1f4e90c146c4cf4210f8258ae4ff65d8c911acc2
Paul Fisher-York <pbfy0@yahoo.com>
2011-05-26 14:49:06 -0400

    added content

---
+++ cddd.js
@@ -0,0 +1,115 @@
+(function(){
+var 
+  b2Vec2 = Box2D.Common.Math.b2Vec2, 
+  b2AABB = Box2D.Collision.b2AABB, 
+  b2Body = Box2D.Dynamics.b2Body, 
+  b2DebugDraw = Box2D.Dynamics.b2DebugDraw, 
+  b2Color = Box2D.Common.b2Color;
+   Box2D.Dynamics.b2World.prototype.DrawDebugData = function () {
+      if (this.m_debugDraw == null) {
+         return;
+      }
+      this.m_debugDraw.m_sprite.graphics.clear();
+      var flags = this.m_debugDraw.GetFlags();
+      var i = 0;
+      var b;
+      var f;
+      var s;
+      var j;
+      var bp;
+      var invQ = new b2Vec2;
+      var x1 = new b2Vec2;
+      var x2 = new b2Vec2;
+      var xf;
+      var col, ud;
+      var b1 = new b2AABB();
+      var b2 = new b2AABB();
+      var vs = [new b2Vec2(), new b2Vec2(), new b2Vec2(), new b2Vec2()];
+      var color = new b2Color(0, 0, 0);
+      if (flags & b2DebugDraw.e_shapeBit) {
+         for (b = this.m_bodyList;
+         b; b = b.m_next) {
+            xf = b.m_xf;
+            for (f = b.GetFixtureList();
+            f; f = f.m_next) {
+               s = f.GetShape();
+	       ud = f.GetUserData();
+	       if(ud !== null){
+	       col = f.GetUserData().color;
+               color.Set(col.r/255, col.g/255, col.b/255);
+	       }
+               else if (b.IsActive() == false) {
+                  color.Set(0.5, 0.5, 0.3);
+//                  this.DrawShape(s, xf, color);
+               }
+               else if (b.GetType() == b2Body.b2_staticBody) {
+                  color.Set(0.5, 0.9, 0.5);
+//                  this.DrawShape(s, xf, color);
+               }
+               else if (b.GetType() == b2Body.b2_kinematicBody) {
+                  color.Set(0.5, 0.5, 0.9);
+//                  this.DrawShape(s, xf, color);
+               }
+               else if (b.IsAwake() == false) {
+                  color.Set(0.6, 0.6, 0.6);
+//                  this.DrawShape(s, xf, color);
+               }
+               else {
+                  color.Set(0.9, 0.7, 0.7);
+//                  this.DrawShape(s, xf, color);
+               }
+                  this.DrawShape(s, xf, color);
+            }
+         }
+      }
+      if (flags & b2DebugDraw.e_jointBit) {
+         for (j = this.m_jointList;
+         j; j = j.m_next) {
+            this.DrawJoint(j);
+         }
+      }
+      if (flags & b2DebugDraw.e_controllerBit) {
+         for (var c = this.m_controllerList; c; c = c.m_next) {
+            c.Draw(this.m_debugDraw);
+         }
+      }
+      if (flags & b2DebugDraw.e_pairBit) {
+         color.Set(0.3, 0.9, 0.9);
+         for (var contact = this.m_contactManager.m_contactList; contact; contact = contact.GetNext()) {
+            var fixtureA = contact.GetFixtureA();
+            var fixtureB = contact.GetFixtureB();
+            var cA = fixtureA.GetAABB().GetCenter();
+            var cB = fixtureB.GetAABB().GetCenter();
+            this.m_debugDraw.DrawSegment(cA, cB, color);
+         }
+      }
+      if (flags & b2DebugDraw.e_aabbBit) {
+         bp = this.m_contactManager.m_broadPhase;
+         vs = [new b2Vec2(), new b2Vec2(), new b2Vec2(), new b2Vec2()];
+         for (b = this.m_bodyList;
+         b; b = b.GetNext()) {
+            if (b.IsActive() == false) {
+               continue;
+            }
+            for (f = b.GetFixtureList();
+            f; f = f.GetNext()) {
+               var aabb = bp.GetFatAABB(f.m_proxy);
+               vs[0].Set(aabb.lowerBound.x, aabb.lowerBound.y);
+               vs[1].Set(aabb.upperBound.x, aabb.lowerBound.y);
+               vs[2].Set(aabb.upperBound.x, aabb.upperBound.y);
+               vs[3].Set(aabb.lowerBound.x, aabb.upperBound.y);
+               this.m_debugDraw.DrawPolygon(vs, 4, color);
+            }
+         }
+      }
+      if (flags & b2DebugDraw.e_centerOfMassBit) {
+         for (b = this.m_bodyList;
+         b; b = b.m_next) {
+            xf = b2World.s_xf;
+            xf.R = b.m_xf.R;
+            xf.position = b.GetWorldCenter();
+            this.m_debugDraw.DrawTransform(xf);
+         }
+      }
+   }
+})();
---
+++ levelset.js
@@ -0,0 +1,44 @@
+var vspace = 1;
+var hspace = 1;
+var wdth = 1;
+var hght = 1;
+
+levelset = [
+	[5,  // width of base
+	[true, -2, 0, 1, 1], // true -> blue, xpos, ypos, width, height
+	[true, -1, 0, 1, 1],
+	[false, 0, 0, 1, 1],
+	[true, 1, 0, 1, 1],
+	[true, 2, 0, 1, 1],
+	[false, -1.5, 1, 1, 1],
+	[true, -0.5, 1, 1, 1],
+	[true, 0.5, 1, 1, 1],
+	[false, 1.5, 1, 1, 1],
+	[false, -1, 2, 1, 1],
+	[true, 0, 2, 1, 1],
+	[false, 1, 2, 1, 1],
+	[true, -0.5, 3, 1, 1],
+	[true, 0.5, 3, 1, 1],
+	[true, 0, 4, 1, 1],
+	[false, 0, 5, 1, 1]],
+	[4,
+	[true, 0, 0, 1, 1]],
+	[5,
+	[false, -2, 0, wdth, hght],
+	[true, -1, 0, wdth, hght],
+	[false, 0, 0, wdth, hght],
+	[true, 1, 0, wdth, hght],
+	[true, -1.6, 1, wdth, hght],
+	[false, -0.6, 1, wdth, hght],
+	[true, 0.4, 1, wdth, hght],
+	[false, 1.4, 1, wdth, hght],
+	[false, -1.2, 2, wdth, hght],
+	[true, -0.2, 2, wdth, hght], 
+	[false, 0.8, 2, wdth, hght],
+	[true, 1.8, 2, wdth, hght],
+	[true, -1.6, 3, wdth, hght],
+	[false, -0.6, 3, wdth, hght],
+	[true, 0.4, 3, wdth, hght],
+	[false, 1.4, 3, wdth, hght]]
+	
+]	
