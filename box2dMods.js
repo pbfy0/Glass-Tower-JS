6e22db8150466da4a8296516fd54b84f7414f461
Paul Fisher-York <pbfy0@yahoo.com>
2011-07-24 18:49:09 -0400

    indentation in code

---
+++ box2dMods.js
@@ -0,0 +1,184 @@
+(function(){
+    var 
+    b2Vec2 = Box2D.Common.Math.b2Vec2, 
+    b2AABB = Box2D.Collision.b2AABB, 
+    b2Body = Box2D.Dynamics.b2Body, 
+    b2DebugDraw = Box2D.Dynamics.b2DebugDraw, 
+    b2Color = Box2D.Common.b2Color;
+    b2Shape = Box2D.Collision.Shapes.b2Shape;
+    b2Math = Box2D.Common.Math.b2Math;
+    Box2D.Dynamics.b2World.prototype.DrawDebugData = function () {
+	if (this.m_debugDraw == null) {
+            return;
+	}
+	this.m_debugDraw.m_sprite.graphics.clear();
+	var flags = this.m_debugDraw.GetFlags();
+	var i = 0;
+	var b;
+	var f;
+	var s;
+	var j;
+	var bp;
+	var invQ = new b2Vec2;
+	var x1 = new b2Vec2;
+	var x2 = new b2Vec2;
+	var xf;
+	var col, ud;
+	var b1 = new b2AABB();
+	var b2 = new b2AABB();
+	var vs = [new b2Vec2(), new b2Vec2(), new b2Vec2(), new b2Vec2()];
+	var color = new b2Color(0, 0, 0);
+	var outlineColor = undefined;
+	if (flags & b2DebugDraw.e_shapeBit) {
+            for (b = this.m_bodyList;
+		 b; b = b.m_next) {
+		xf = b.m_xf;
+		for (f = b.GetFixtureList();
+		     f; f = f.m_next) {
+		    s = f.GetShape();
+		    ud = f.GetUserData();
+		    if(ud !== null){
+			if(ud.outlineColor !== undefined){
+			    outlineColor = new b2Color();
+			    col = ud.outlineColor;
+			    outlineColor.Set(col.r/255, col.g/255, col.b/255);
+			    //		 ud.outlineColor;
+			}
+			if(ud.color === undefined){
+			    ud = null;
+			}
+		    }
+		    if(ud !== null){
+			if(ud.color !== undefined){
+			    col = ud.color;
+			    color.Set(col.r/255, col.g/255, col.b/255);
+			}
+		    }
+		    else if (b.IsActive() == false) {
+			color.Set(0.5, 0.5, 0.3);
+			//                  this.DrawShape(s, xf, color);
+		    }
+		    else if (b.GetType() == b2Body.b2_staticBody) {
+			color.Set(0.5, 0.9, 0.5);
+			//                  this.DrawShape(s, xf, color);
+		    }
+		    else if (b.GetType() == b2Body.b2_kinematicBody) {
+			color.Set(0.5, 0.5, 0.9);
+			//                  this.DrawShape(s, xf, color);
+		    }
+		    else if (b.IsAwake() == false) {
+			color.Set(0.6, 0.6, 0.6);
+			//                  this.DrawShape(s, xf, color);
+		    }
+		    else {
+			color.Set(0.9, 0.7, 0.7);
+			//                  this.DrawShape(s, xf, color);
+		    }
+                    this.DrawShape(s, xf, color, outlineColor);
+		}
+            }
+	}
+	if (flags & b2DebugDraw.e_jointBit) {
+            for (j = this.m_jointList;
+		 j; j = j.m_next) {
+		this.DrawJoint(j);
+            }
+	}
+	if (flags & b2DebugDraw.e_controllerBit) {
+            for (var c = this.m_controllerList; c; c = c.m_next) {
+		c.Draw(this.m_debugDraw);
+            }
+	}
+	if (flags & b2DebugDraw.e_pairBit) {
+            color.Set(0.3, 0.9, 0.9);
+            for (var contact = this.m_contactManager.m_contactList; contact; contact = contact.GetNext()) {
+		var fixtureA = contact.GetFixtureA();
+		var fixtureB = contact.GetFixtureB();
+		var cA = fixtureA.GetAABB().GetCenter();
+		var cB = fixtureB.GetAABB().GetCenter();
+		this.m_debugDraw.DrawSegment(cA, cB, color);
+            }
+	}
+	if (flags & b2DebugDraw.e_aabbBit) {
+            bp = this.m_contactManager.m_broadPhase;
+            vs = [new b2Vec2(), new b2Vec2(), new b2Vec2(), new b2Vec2()];
+            for (b = this.m_bodyList;
+		 b; b = b.GetNext()) {
+		if (b.IsActive() == false) {
+		    continue;
+		}
+		for (f = b.GetFixtureList();
+		     f; f = f.GetNext()) {
+		    var aabb = bp.GetFatAABB(f.m_proxy);
+		    vs[0].Set(aabb.lowerBound.x, aabb.lowerBound.y);
+		    vs[1].Set(aabb.upperBound.x, aabb.lowerBound.y);
+		    vs[2].Set(aabb.upperBound.x, aabb.upperBound.y);
+		    vs[3].Set(aabb.lowerBound.x, aabb.upperBound.y);
+		    this.m_debugDraw.DrawPolygon(vs, 4, color);
+		}
+            }
+	}
+	if (flags & b2DebugDraw.e_centerOfMassBit) {
+            for (b = this.m_bodyList;
+		 b; b = b.m_next) {
+		xf = b2World.s_xf;
+		xf.R = b.m_xf.R;
+		xf.position = b.GetWorldCenter();
+		this.m_debugDraw.DrawTransform(xf);
+            }
+	}
+    }
+
+    Box2D.Dynamics.b2World.prototype.DrawShape = function (shape, xf, color, outlineColor) {
+	switch (shape.m_type) {
+	case b2Shape.e_circleShape:
+            {
+		var circle = ((shape instanceof b2CircleShape ? shape : null));
+		var center = b2Math.MulX(xf, circle.m_p);
+		var radius = circle.m_radius;
+		var axis = xf.R.col1;
+		this.m_debugDraw.DrawSolidCircle(center, radius, axis, color);
+            }
+            break;
+	case b2Shape.e_polygonShape:
+            {
+		var i = 0;
+		var poly = ((shape instanceof b2PolygonShape ? shape : null));
+		var vertexCount = parseInt(poly.GetVertexCount());
+		var localVertices = poly.GetVertices();
+		var vertices = new Vector(vertexCount);
+		for (i = 0;
+		     i < vertexCount; ++i) {
+		    vertices[i] = b2Math.MulX(xf, localVertices[i]);
+		}
+		this.m_debugDraw.DrawSolidPolygon(vertices, vertexCount, color, outlineColor);
+            }
+            break;
+	case b2Shape.e_edgeShape:
+            {
+		var edge = (shape instanceof b2EdgeShape ? shape : null);
+		this.m_debugDraw.DrawSegment(b2Math.MulX(xf, edge.GetVertex1()), b2Math.MulX(xf, edge.GetVertex2()), color);
+            }
+            break;
+	}
+    }
+
+    b2DebugDraw.prototype.DrawSolidPolygon = function (vertices, vertexCount, color, outlineColor) {
+	if(!vertexCount) return;
+	var s = this.m_ctx;
+	var drawScale = this.m_drawScale;
+	s.beginPath();
+	if(outlineColor === undefined){outlineColor = color}
+	s.strokeStyle = this._color(outlineColor.color, this.m_alpha);
+	s.fillStyle = this._color(color.color, this.m_fillAlpha);
+	s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+	for (var i = 1; i < vertexCount; i++) {
+            s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+	}
+	s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+	s.closePath();
+	s.fill();
+	s.stroke();
+    };
+
+})();
